8019f43bacbd73cc7392a94f6be8301b90529f59
imerle <i.merle@erival.fr>
2025-01-20 17:17:10 +0100

    validation folders

--- validation/validation.py
+++ validation/validation.py
@@ -10,10 +10,9 @@ for file in listdir('examples'):
     with open(f'examples/{file}') as f:
         example = json.load(f)
     if '$schema' in example:
-        #resp = requests.get(url=example['$schema'])
-        with open(f'json_schema/{file.split('.')[0]}.schema.json') as f_schema:
-            schema = json.load(f_schema)
-            validate(instance=example, schema=schema)
-            print(f"le fichier {file} est valide")
+        resp = requests.get(url=example['$schema'])
+        schema = resp.json()
+        validate(instance=example, schema=schema)
+        print(f"le fichier {file} est valide")
     else:
         raise Exception(f"le fichier {file} ne précise pas de schéma") 
