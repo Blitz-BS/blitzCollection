44e49834dfe051fea280d2101c96115490e5fe60
imerle <i.merle@erival.fr>
2025-01-22 18:09:24 +0100

    URI relative et ajouts des codebiteurs/cautions

--- validation/validation.py
+++ validation/validation.py
@@ -1,18 +1,19 @@
 """
 Ce script valide des fichiers JSON dans le répertoire "examples" en utilisant les schémas JSON spécifiés dans chaque fichier.
 """
-from jsonschema import validate
 import json
-from os import listdir
 import requests
+from jsonschema import validate
+from os import listdir  
+
 
 for file in listdir('examples'):
     with open(f'examples/{file}', encoding='utf-8') as f:
         example = json.load(f)
     if '$schema' in example:
         resp = requests.get(url=example['$schema'])
-        schema = resp.json()        
+        schema = resp.json()
         validate(instance=example, schema=schema)
         print(f"le fichier {file} est valide")
     else:
-        raise Exception(f"le fichier {file} ne précise pas de schéma") 
+        raise Exception(f"le fichier {file} ne précise pas de schéma")
